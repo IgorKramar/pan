3810803c85875aa7ac7e2aad2fe39416b5d48be6
Igor Kramar <i@ikramar.ru>
2024-09-03 15:25:21 +0600

    fix: remove refactor

--- src/commands/sumcode.rs
+++ src/commands/sumcode.rs
@@ -1,4 +1,4 @@
-use crate::filesystem::directory_tree::{DirectoryTree, CollectOptions};
+use crate::filesystem::directory_tree::DirectoryTree;
 use crate::filesystem::file_writer::write_to_file;
 use crate::cli::args::SumcodeArgs;
 
@@ -14,6 +14,7 @@ pub fn sumcode_command(args: &SumcodeArgs) {
 
     let (exclude_paths, exclude_patterns) = split_paths_and_patterns(&args.exclude);
 
+    // Печатаем исключаемые пути и паттерны
     if !exclude_paths.is_empty() {
         println!("Exclude paths:");
         for path in &exclude_paths {
@@ -35,23 +36,31 @@ pub fn sumcode_command(args: &SumcodeArgs) {
         exclude_patterns,
     );
 
-    let options = CollectOptions::new(
-        true, // Собираем структуру
-        args.with_file_content.is_some(), // Включаем содержимое файлов, если указаны расширения
-        args.with_file_content.clone(),
-    );
+    // Сначала собираем структуру проекта
+    tree.collect_structure(&mut output_content);
 
-    tree.collect(&mut output_content, options);
+    // Если указаны расширения, разбираем их и добавляем содержимое файлов после структуры
+    if let Some(ref extensions) = args.with_file_content {
+        let extensions: Vec<String> = extensions.iter()
+            .flat_map(|ext| ext.split(','))
+            .map(|ext| ext.trim().to_string())
+            .collect();
 
+        tree.collect_file_contents(&mut output_content, &extensions);
+    }
+
+    // Если указан output, записываем в файл, иначе выводим в консоль
     if let Some(ref output_path) = args.output {
         if let Err(e) = write_to_file(output_path, &output_content) {
             eprintln!("Error writing to file: {}", e);
         }
     } else {
+        // Выводим в консоль
         println!("{}", output_content);
     }
 }
 
+/// Разделяет строку на пути и паттерны
 fn split_paths_and_patterns(paths: &Vec<String>) -> (Vec<String>, Vec<String>) {
     let mut exclude_paths = Vec::new();
     let mut exclude_patterns = Vec::new();
